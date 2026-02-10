b1bad124f26705eb771c7f4d10fd8ca5b7d62bb0
Qusai Haddad <qusaihaddad06@gmail.com>
2026-02-10 14:24:08 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
+// Toggle code block visibility
+function toggleCode(id) {
+  const code = document.getElementById(id);
+  if (!code) return;
+  
+  code.style.display = (code.style.display === "block") ? "none" : "block";
+}
 
